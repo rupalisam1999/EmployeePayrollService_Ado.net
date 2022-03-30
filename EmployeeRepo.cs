a29103992dd5145546c19e90fc40bd3108f2590b
rupalisam1999 <rupalisangale1999@gmail.com>
2022-03-30 13:49:26 +0530

    [Rupali]ADDED: Welcome to EmployrrPayrollService

---
+++ EmployeeRepo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollService_Ado.net
+{
+    class EmployeeRepo
+    {
+        private static string connectionString = @"Data Source=(Localdb)\MSSQLLocalDB;Initial Catalog=payroll_service;Integrated Security=True;";
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        public void checkConnection()
+        {
+            try
+            {
+                this.connection.Open();
+                Console.WriteLine("connection established");
+                this.connection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmployeePayrollService_Ado.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcomr to the Employee payroll service ado .net program");
+            EmployeeRepo employeeRepo = new EmployeeRepo();
+            employeeRepo.checkConnection();
+        }
+    }
+}
